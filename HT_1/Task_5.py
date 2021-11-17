876fab3e01197804a98d3dace238b4af917e1636
FAQsUA <faqs.ua@gmail.com>
2021-11-17 10:49:52 -0800

    add Task_5 / complite

---
+++ HT_1/Task_5.py
@@ -0,0 +1,11 @@
+dec_nums = input('Enter Dec Number --> ')
+# create tuple from enter dec numbers
+dec_list = tuple(dec_nums.split(','))
+hex_list = ()
+
+for i in dec_list:
+    # Add new number to tuple hex_list and convert dec to hex
+    hex_list += (hex(int(i)),)
+    continue
+
+print('Out Hex Numbers --> {0}'.format(','.join(hex_list)))
