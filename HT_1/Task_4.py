a01fe0d74910644071d3b27aad5888cbf4689f5f
FAQsUA <faqs.ua@gmail.com>
2021-11-17 10:49:10 -0800

    add Task_4 / complite

---
+++ HT_1/Task_4.py
@@ -0,0 +1,16 @@
+num = int(input('Enter number of strings -->' ))
+str_list = []
+concat_string = ''
+i = 0
+# add element to list
+for i in range(0, num):
+    str_list.append(input('Enter string ' + str(i+1) + ' -->'))
+# concat strings from list
+    concat_string = concat_string + str_list[i] 
+    i += 1
+    continue
+# check for empty string
+if not concat_string:
+    print('Empty string')
+else:
+    print('"'+concat_string+'"')
